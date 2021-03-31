1b5d68b63aae998ff8a739fba9f576979902ecfa
DominiK K <48619420+maxim093@users.noreply.github.com>
2021-03-31 22:11:05 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <Button>Klick mich</Button>
+</template>
+
+<script>
+import Button from "./atoms/Button";
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <router-link v-if="href" :href="href" class="Btn Btn--green"
+      ><slot
+    /></router-link>
+    <button v-else class="Btn Btn--green"><slot /></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: null,
+    },
+    variant: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.Btn {
+  border-radius: 5px;
+  color: black;
+  width: 15vw;
+  height: 5vh;
+  font-size: 16px;
+  font-weight: 600;
+
+  &--green {
+    background-color: darkslategray;
+    border: 4px solid palegreen;
+    color: #fff;
+  }
+
+  &--pink {
+    background-color: palevioletred;
+    border: 4px solid peachpuff;
+    color: #fff;
+  }
+  &--black {
+    background-color: black;
+    border: 4px solid black;
+    color: #fff;
+  }
+  &--white {
+    background-color: white;
+    border: 4px solid black;
+  }
+}
+</style>
